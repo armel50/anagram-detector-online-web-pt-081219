538eabbaafa121008c72702a6e08a1d5449ee7a1
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-18 13:59:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,19 @@
 class Anagram
-  attr_accessor :word
+  attr_accessor :word, :array_of_anagrams
+  
+  @array_of_anagrams = []
   
   def initialize(word)
-    @word = word
+    @word = word 
+    @splited_word = word.split("").sort
   end
   
   def match(possible_anagram)
-   
+    possible_anagram.each do |el|
+      splited_el = el.split("").sort
+      @array_of_anagrams << el if @splited_word == splited_el
+      
+    end   
   end
   
 end
