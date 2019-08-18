69788c5efd399492e232a97eb631abcf60a51f6d
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-18 14:04:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
     possible_anagram.each do |el|
       splited_el = el.split("").sort
      
-      puts @array_of_anagrams if @splited_word == splited_el
+      p @array_of_anagrams if @splited_word == splited_el
       
     end   
   end
