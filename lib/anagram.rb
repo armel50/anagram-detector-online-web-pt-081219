71c845d67482886db8f483d607cfaf476380afb2
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-18 13:42:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,14 @@
 class Anagram
+  attr_accessor :word
   
   def initialize(word)
     @word = word
+  end
+  
+  def match(possible_anagram)
+    possible_anagram.each do |el|
+      el.all
+    end
   end
   
 end
